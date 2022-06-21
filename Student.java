e4a9260a980cde1278425080db956ab5e1e12ad4
Chandan Kumar <99543778+RavSinghChandan@users.noreply.github.com>
2022-06-21 11:48:07 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,10 @@
+package com.q1;
+
+public class Student {
+	public Student(int i, String string, int j) {
+		// TODO Auto-generated constructor stub
+	}
+	int studentId ; 
+	String courseEnrolled ; 
+	int courseFee ; 
+}
