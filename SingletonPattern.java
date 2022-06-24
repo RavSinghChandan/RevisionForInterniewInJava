e955253c95308a46a5dee495362d43ceea0675c0
Chandan Kumar <99543778+RavSinghChandan@users.noreply.github.com>
2022-06-24 16:58:03 +0530

    Singleton Pattern code in Java

---
+++ SingletonPattern.java
@@ -0,0 +1,13 @@
+package singletonPatterByEnum;
+
+public class SingletonPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("obj");
+
+	}
+
+	
+
+}
